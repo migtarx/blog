2b344bcf798e810aa6b9b4938d10bb7797734e13
Miguel Puerta <miguel@mpuerta.dev>
2023-10-30 12:44:05 +0100

    feat: adds express server

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening in port ${PORT}`);
+});
