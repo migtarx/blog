e24a07c170acd777c5231a7b8387696f11fec8ec
Miguel Puerta <miguel@mpuerta.dev>
2023-12-17 18:20:09 +0100

    feat: adds deploy confirmation notifications

--- src/scripts/notifyDeploy.js
+++ src/scripts/notifyDeploy.js
@@ -2,17 +2,18 @@ require('dotenv').config();
 const deployEnv = process.env.DEPLOY_ENV;
 const ntfyUser = process.env.NTFY_USER;
 const ntfyPasswd = process.env.NTFY_PASSWD;
-const ntfyTopicUrl = process.env.NTFY_TOPIC_URL;
+const ntfyServer = process.env.NTFY_SERVER_URL;
 const projectName = process.env.PROJECT_NAME;
+const fullUrl = `${ntfyServer}/${projectName}`;
 const encodedAuth = Buffer.from(`${ntfyUser}:${ntfyPasswd}`).toString('base64');
 
 (async function() {
-    await fetch(ntfyTopicUrl, {
+    await fetch(fullUrl, {
         method: 'POST',
         body: `${projectName} successfully deployed to ${deployEnv}`,
         headers: {
             'Authorization': `Basic ${encodedAuth}`,
-            'Title': `Sucessfull deploy - [${projectName}]`,
+            'Title': `Successful deploy - [${projectName}]`,
             'Tags': 'white_check_mark'
         }
     })
